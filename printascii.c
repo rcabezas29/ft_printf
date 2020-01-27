e9a570233ad293dc7fa95abf8d5f93dd885f032c
Raul Cabezas gonzalez <rcabezas@c2r17s3.42madrid.com>
2020-01-27 21:07:42 +0100

    Pasando los tests

--- printascii.c
+++ printascii.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 16:41:23 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/01/23 20:08:35 by rcabezas         ###   ########.fr       */
+/*   Updated: 2020/01/27 21:00:55 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ static void	ft_putpads(t_struct *ps, int less, int arglen)
 	int	widthlen;
 
 	i = 0;
-	if (ps->precision > 0 && ps->precision < arglen)
+	if (ps->precision != 0 && ps->precision != 0)
 	{
-		widthlen = ps->width - ps->precision;
+		widthlen = ps->precision;
 	}
 	else
-		widthlen = ps->width - arglen;
+		widthlen = arglen;
 	if (ps->precision == -1)
 		widthlen += arglen;
-	if (ps->width && ((!less && ps->flags[2] == 1) ||
-				(less && ps->flags[2] != 1)))
+	if (ps->width == 1 && ((less == 0 && ps->flags[2] == 1) ||
+				(less != 0 && ps->flags[2] != 1)))
 	{
 		while (++i <= widthlen)
 			ft_putchar(' ');
@@ -41,10 +41,10 @@ void		ft_printchar(t_struct *ps, char arg)
 	int	i;
 
 	i = 0;
-	if (ps->width || ps->precision)
+	if (ps->width != 0 || ps->precision != 0)
 		ft_putpads(ps, 1, 1);
 	ft_putchar((char)arg);
-	if (ps->width && ps->flags[2] == 1)
+	if (ps->width != 0 && ps->flags[2] == 1)
 		ft_putpads(ps, 0, 1);
 	ps->ret += 1;
 }
@@ -54,11 +54,9 @@ void		ft_printstr(t_struct *ps, char *arg)
 	int	i;
 
 	i = 0;
-	printf("Precision = %i\n", ps->precision);
-	printf("Width = %i\n", ps->width);
 	if (!arg)
 		arg = "(null)";
-	if (ps->width || ps->precision)
+	if (ps->width != 0 || ps->precision != 0)
 		ft_putpads(ps, 1, ft_strlen(arg));
 	if (ps->precision != 0 && ps->precision != -1)
 	{
@@ -71,6 +69,6 @@ void		ft_printstr(t_struct *ps, char *arg)
 		ft_putstr(arg);
 		ps->ret += ft_strlen(arg);
 	}
-	if (ps->width && ps->flags[2] == 1)
+	if (ps->width != 0 && ps->flags[2] == 1)
 		ft_putpads(ps, 0, ft_strlen(arg));
 }
